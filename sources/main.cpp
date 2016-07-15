8cdfef32d25bc7075013a6928b5f723d3f93605e
SouthRa <ko19951231@gmail.com>
2016-07-15 17:04:16 +0800

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -19,6 +19,14 @@ int main(int argv, char **argc) {
 	}
 	ISC_parser testdata;
 	circuit ori_cir = testdata.parse_isc_file(argc[1]);
-
+	ori_cir.print_circuit();
+	circuit cir_1;
+	circuit cir_2;
+	int mode, id;
+	scanf("%d %d", &mode, &id);
+	cir_1=ori_cir.insert_fault(mode, id);
+	scanf("%d %d", &mode, &id);
+	cir_2=ori_cir.insert_fault(mode, id);
+	beq(cir_1, cir_2);
 	return 0;
 }
