edd2d76149323d718a563562b6c723e6b8bcb652
SouthRa <ko19951231@gmail.com>
2016-07-22 14:43:19 +0800

    Update circuit.cpp
    add dfs function

--- sources/circuit.cpp
+++ sources/circuit.cpp
@@ -26,6 +26,7 @@ void Circuit::insert_output(int out)// to record which gates are primary outputs
 {
     out=gate_trans(out);
     output.push_back(out);
+    //circuit[out]
 }
 
 void Circuit::insert_gate(int mode, int in1, int in2, int out)
@@ -45,6 +46,7 @@ void Circuit::insert_gate(int mode, int in1, int in2, int out)
     n.in2=in2;
     n.out=out;
     n.neg=n.sa0=n.sa1=0;
+
     //circuit.push_back(n);
     circuit[out]=n;
 }
@@ -56,31 +58,71 @@ void Circuit::insert_fault(int mode, int id)
     else{
         if(mode==0){//stuck at 0
             circuit[id].sa0=1;
-            circuit[id].sa1=0;
         }
         else if(mode==1){//stuck at 1
             circuit[id].sa1=1;
-            circuit[id].sa0=0;
         }
         else if(mode==10){// negation
-            circuit[id].neg^=1;
-            if(circuit[id].sa0){
-                circuit[id].sa0=0;
-                circuit[id].sa1=1;
+            circuit[id].neg=1;
+        }
+        if(mode==0 || mode==1){//stuck at 0 or 1
+            if(circuit[id].mode==0){//primary input
+                node n;
+                n.in1=id;
+                n.in2=-1;
+                n.out=cnt+1;
+                n.mode=8;
+                if(mode==0) n.sa0=1;
+                else n.sa1=1;
+                circuit.push_back(n);
+                cnt++;
+                circuit[id].sa0=circuit[id].sa1=0;
+                set<int>::iterator it;
+                for(it=circuit[id].fanout.begin();it!=circuit[id].fanout.end();it++){
+                    int fout=*it;
+                    circuit[cnt].fanout.insert(fout);
+                    circuit[fout].fanin.insert(cnt);
+                }
             }
-            else if(circuit[id].sa1){
-                circuit[id].sa1=0;
-                circuit[id].sa0=1;
-            }
+        }
+    }
+}
+
+void Circuit::dfs()
+{
+    for(int i=0;i<output.size();i++){
+        stack<int> s;
+        int index=output[i];
+        int cur=output[i];
+        node n=circuit[cur];
+        s.push(cur);
+        while(!s.empty()){
+            cur=s.top();
+            s.pop();
+            printf("cur=%d\n", cur);
+            if(circuit[index].fanin.find(cur)!=circuit[index].fanin.end()) continue;
+            circuit[index].fanin.insert(cur);
+            circuit[cur].fanout.insert(index);
+            int n1=circuit[cur].in1;
+            int n2=circuit[cur].in2;
+            if(n1>0) s.push(n1);
+            if(n2>0) s.push(n2);
         }
     }
 }
 
 void Circuit::print_circuit()
 {
+    set<int>::iterator it;
     printf("cnt=%d input_cnt=%d gate_cnt=%d\n", cnt, input_cnt, gate_cnt);
+    printf("circuit.size=%d\n", circuit.size());
     for(int i=1;i<circuit.size();i++){
         printf("mode=%d in1=%d in2=%d out=%d neg=%d sa0=%d sa1=%d i=%d\n", circuit[i].mode, circuit[i].in1, circuit[i].in2, circuit[i].out, circuit[i].neg, circuit[i].sa0, circuit[i].sa1, i);
+        printf("fanin: ");
+        for(it=circuit[i].fanin.begin();it!=circuit[i].fanin.end();it++) printf("%d ", *it);
+        printf("\nfanout: ");
+        for(it=circuit[i].fanout.begin();it!=circuit[i].fanout.end();it++) printf("%d ", *it);
+        puts("");
     }
     for(int i=0;i<output.size();i++){
         printf("output %d\n", output[i]);
