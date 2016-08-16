1b8b7d1a671f1a87fbee8471fe33dcbc2b368324
Sunset <s2821d3721@gmail.com>
2016-08-16 12:16:43 +0800

    Store all fault circuit

--- sources/main.cpp
+++ sources/main.cpp
@@ -13,28 +13,17 @@
 
 using namespace Minisat;
 
+vector<Circuit> cir;
 
 void compare_all(const Circuit &ori_cir, Fault &faults, const vector<int> &slot) {
 
     for(int i=0; i<slot.size(); ++i) {
-
-        int mode = faults[slot[i]].mode;
-        int net = faults[slot[i]].net;
-
-        Circuit cir_1 = ori_cir;
-        cir_1.insert_fault(mode, net);
         
         for(int j=i+1; j<slot.size(); ++j) {
             if( faults.same(slot[i], slot[j]) || faults.diff(slot[i], slot[j]) )
                 continue;
 
-            mode = faults[slot[j]].mode;
-            net = faults[slot[j]].net;
-            
-            Circuit cir_2 = ori_cir;
-            cir_2.insert_fault(mode, net);
-
-            if( beq(cir_1, cir_2) )
+            if( beq(cir[slot[i]], cir[slot[j]]) )
                 faults.setSame(slot[i], slot[j]);
             else
                 faults.setDiff(slot[i], slot[j]);
@@ -49,10 +38,7 @@ void procTrashFault(const Circuit &ori_cir, Fault &faults, vector<bool> &trash) 
     // Find all trush fault(same as ori_cir)
     int trashLeader = -1;
     for(int i=0; i<faults.size(); ++i) {
-        Circuit cir = ori_cir;
-        cir.insert_fault(faults[i].mode, faults[i].net);
-
-        if( beq(cir, ori_cir) ) {
+        if( beq(cir[i], ori_cir) ) {
             trash[i] = true;
             if( trashLeader==-1 )
                 trashLeader = i;
@@ -71,21 +57,12 @@ void procTrashFault(const Circuit &ori_cir, Fault &faults, vector<bool> &trash) 
 
 void procRandomSimulationTest(const Circuit &ori_cir, Fault &faults, const vector<bool> &trash) {
     
-    struct Item{
-        int fid, cid;
-    };
-    vector< vector<Item> > slot(1);
+    vector< vector<int> > slot(1);
     
     // Init
     for(int i=0; i<faults.size(); ++i)
         if( !trash[i] )
-            slot[0].push_back({i, -1});
-
-    vector<Circuit> cir(slot[0].size(), ori_cir);
-    for(int i=0; i<slot[0].size(); ++i) {
-        cir[i].insert_fault(faults[slot[0][i].fid].mode, faults[slot[0][i].fid].net);
-        slot[0][i].cid = i;
-    }
+            slot[0].push_back(i);
 
     // Run simulation test
     vector<bool> input(ori_cir.input_cnt, 0);
@@ -103,12 +80,12 @@ void procRandomSimulationTest(const Circuit &ori_cir, Fault &faults, const vecto
                 continue;
 
             // pick slot[i][0] as leader
-            vector<bool> leaderBits = simulate(cir[slot[i][0].cid], input, dff);
+            vector<bool> leaderBits = simulate(cir[slot[i][0]], input, dff);
             
             // all fault different from leader must exit
-            vector<Item> byebye;
+            vector<int> byebye;
             for(int j=1,top=1; j<slot[i].size(); ++j) {
-                vector<bool> nowBits = simulate(cir[slot[i][j].cid], input, dff);
+                vector<bool> nowBits = simulate(cir[slot[i][j]], input, dff);
                 if( nowBits != leaderBits )
                     byebye.emplace_back(slot[i][j]);
                 else
@@ -133,7 +110,7 @@ void procRandomSimulationTest(const Circuit &ori_cir, Fault &faults, const vecto
         for(auto &id1 : slot[i])
             for(int j=0; j<i; ++j)
                 for(auto &id2 : slot[j])
-                    faults.setDiff(id1.fid, id2.fid);
+                    faults.setDiff(id1, id2);
 }
 
 
@@ -153,13 +130,18 @@ int main(int argv, char **argc) {
     Circuit ori_cir = testdata.parse_isc_file(argc[1]);
     Fault faults = testdata.parse_fault_file(argc[2]);
 
+
     ori_cir.dfs();
 
 
+    cir.resize(faults.size(), ori_cir);
+    for(int i=0; i<faults.size(); ++i)
+        cir[i].insert_fault(faults[i].mode, faults[i].net);
     
-    vector<bool> trash(faults.size(), false);
 
+    vector<bool> trash(faults.size(), false);
     procTrashFault(ori_cir, faults, trash);
+
 
     procRandomSimulationTest(ori_cir, faults, trash);
     int trashcnt = 0;
@@ -169,7 +151,7 @@ int main(int argv, char **argc) {
     printf("%d\n", trashcnt);
     printf("%d vs. %d\n", faults.size(), checkSum);
 
-/*
+
     // Greedy partition falut into multiple slot
     vector< vector<int> > slot;
     const vector<node> &cir = ori_cir.circuit;
@@ -211,6 +193,6 @@ int main(int argv, char **argc) {
     for(int i=0; i<ans.size(); ++i)
         printf("%d %d\n", ans[i].first, ans[i].second);
     fflush(stdout);
-*/
+
     return 0;
 }
